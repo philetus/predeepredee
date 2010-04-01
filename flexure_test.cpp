f607c95095de318bf56c2ab3d43dae446151ed55
michael philetus weller <philetus@gmail.com>
2010-04-01 17:21:32 -0400

    got plates connected more or less sanely with flexure

--- flexure_test.cpp
+++ flexure_test.cpp
@@ -38,11 +38,31 @@ int main(int, char**)
         Vector3(x - ur, y, z + vr),
         Vector3(x + ur, y, z + vr),
         color,
-        1.0, // mass
+        0.05, // mass
         3, // u resolution
         9, // v resolution
-        0); // fixed corners - 1 | 2 | 4 | 8
+        0); // fixed corners - 1 | 2 | 4 | 8 
     world.welcome(&flexure);
+    
+    // *** adjust flexure stiffness
+    btSoftBody* f_body = flexure.get_soft_body();
+    btSoftBody::Material* f_material = f_body->appendMaterial();
+    f_material->m_kLST = 0.9; // Linear stiffness coefficient [0,1]
+    f_material->m_kAST = 0.9; // Area/Angular stiffness coefficient [0,1]
+    //f_material->m_kVST; // Volume stiffness coefficient [0,1]
+    f_body->generateBendingConstraints(2, f_material);
+    
+    // *** adjust soft body config
+    f_body->m_cfg.kAHR = 0.9; // anchor stiffness
+    f_body->m_cfg.kCHR = 0.9; // rigid contact stiffness
+    
+    // *** soft body collision mask
+    f_body->m_cfg.collisions =
+        btSoftBody::fCollision::SDF_RS // rigid vs soft: (shape diameter fnctn?)
+        //btSoftBody::fCollision::CL_RS // rigid vs soft: cluster vs convex
+        | btSoftBody::fCollision::VF_SS; // soft vs soft: vertex vs face
+        //| btSoftBody::fCollision::CL_SS // soft vs soft: cluster vs cluster
+        //| btSoftBody::fCollision::CL_SELF; // soft body self collision
     
     // create a box
     // start box in the air
@@ -54,22 +74,24 @@ int main(int, char**)
     Transformation3 box_b_world_frame(box_orientation, box_b_position);
     Transformation3 box_c_world_frame(box_orientation, box_c_position);
 
-    // 10mm x 10mm x 20mm tall oblong box weighing 2 grams
+    // 10mm x 10mm x 20mm tall oblong box weighing 2 (kilo?)grams
     float box_color[4] = {0.2, 0.2, 0.7, 1.0};
     float shoot_box_color[4] = {0.0, 0.7, 0.7, 1.0};
-    Box box_a(Vector3(24.0, 2.0, 16.0), box_a_world_frame, box_color, 0.5);
-    Box box_b(Vector3(24.0, 2.0, 16.0), box_b_world_frame, box_color, 0.5);
+    Box box_a(Vector3(24.0, 2.0, 16.0), box_a_world_frame, box_color, 0.1);
+    Box box_b(Vector3(24.0, 2.0, 16.0), box_b_world_frame, box_color, 0.1);
     Box box_c(Vector3(30.0, 10.0, 30.0), box_c_world_frame, shoot_box_color, 0.5);
 
     world.welcome(&box_a);
     world.welcome(&box_b);
     world.welcome(&box_c);
+        
+    cout << "flexure body has " << f_body->m_nodes.size() << " nodes" << endl;
+    
+    f_body->appendAnchor(24, box_a.get_rigid_body());
+    f_body->appendAnchor(0, box_a.get_rigid_body());
+	f_body->appendAnchor(2, box_b.get_rigid_body());
+	f_body->appendAnchor(26, box_b.get_rigid_body());
 
-    flexure.get_soft_body()->appendAnchor(0, box_a.get_rigid_body());
-	flexure.get_soft_body()->appendAnchor(8, box_a.get_rigid_body());
-
-    flexure.get_soft_body()->appendAnchor(10, box_b.get_rigid_body());
-	flexure.get_soft_body()->appendAnchor(20, box_b.get_rigid_body());
 
     // create window onto world and start event loop
     cout << "making window" << "\n";
